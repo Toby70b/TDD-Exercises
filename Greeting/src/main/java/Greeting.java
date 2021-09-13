4996a8e3cabc4805bf8050dff668b461c878370b
Toby <tobypl407@googlemail.com>
2021-09-13 20:20:33 +0100

    Javadoc and README

--- Greeting/src/main/java/Greeting.java
+++ Greeting/src/main/java/Greeting.java
@@ -4,8 +4,34 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ * Class containing the ability to greet users by name
+ */
 public class Greeting {
 
+    /**
+     * returns a greeting string containing the names passed in the parameter, in the form "Hello,
+     * {@literal <}name{@literal >}."<br/><br/>
+     *
+     * If null is passed it will return a default greeting "Hello, my friend"<br/><br/>
+     *
+     * If multiple values are passed it will return each seperated with an oxford comma.
+     * The final name will also be prefixed with "and". For example "Hello, Billy, Tony, and Emily"<br/><br/>
+     *
+     * If a name is fully capitalised then a shout greeting will be returned instead. A shout is
+     * of the format "HELLO {@literal <}name{@literal >}! <br/><br/>
+     *
+     * Capitalised and non-capitalised names can be used in the same array. This will be treated as a "mixed" greeting.
+     * Mixed Greetings will first greet the non-capitalised names before "shouting" the capitalised names. For example
+     * "Hello, Amy and Charlotte. AND HELLO BRIAN AND TOM!"<br/><br/>
+     *
+     * If a name contains a comma the name will be split with the comma acting as the delimiter<br/><br/>
+     *
+     * Strings surrounded in escaped double quotes escaped will be treated as a single name regardless of commas
+     *
+     * @param names an array of string representing the names to be greeted
+     * @return a String greeting formatted with the rules declared above
+     */
     public String greet(String[] names) {
         if (names == null) {
             return saySingleName("my friend");
@@ -26,6 +52,12 @@ public class Greeting {
                 : getLowerCaseGreeting(lowerCaseNames) + getUpperCaseGreeting(upperCaseNames);
     }
 
+    /**
+     * Strips quotes and splits commas of strings so they dont appear in the greeting
+     *
+     * @param namesArr an array of strings to be sanitised
+     * @return an array of strings split by comma (unless escaped) and with quotes removed
+     */
     private List<String> sanitiseNamesList(String[] namesArr) {
         Predicate<String> stringSurroundedByQuotes = (s -> s.startsWith("\"") && s.endsWith("\""));
         Predicate<String> stringIncludesCommas = (s -> s.contains(","));
@@ -43,6 +75,12 @@ public class Greeting {
         return newNamesList;
     }
 
+    /**
+     * Returns a shout greeting depending on how many names are required to be shouted
+     *
+     * @param upperCaseNames an List of uppercase strings to shout
+     * @return a shout greeting containing the names from the List parameter
+     */
     private String getUpperCaseGreeting(List<String> upperCaseNames) {
         if (upperCaseNames.isEmpty()) {
             return "";
@@ -51,10 +89,16 @@ public class Greeting {
         } else if (upperCaseNames.size() == 2) {
             return "HELLO " + upperCaseNames.get(0) + " AND " + upperCaseNames.get(1) + "!";
         }
-        return buildUpperCaseGreeting(upperCaseNames);
+        return buildShoutGreeting(upperCaseNames);
     }
 
-    private String buildUpperCaseGreeting(List<String> upperCaseNames) {
+    /**
+     * Builds an shout greeting for more multiple names
+     *
+     * @param upperCaseNames an array of uppercase strings to be included in the greeting
+     * @return a String representing the shout greeting
+     */
+    private String buildShoutGreeting(List<String> upperCaseNames) {
         StringBuilder stringBuilder = new StringBuilder("HELLO ");
         String lastName = upperCaseNames.remove(upperCaseNames.size() - 1);
         upperCaseNames.forEach(name -> stringBuilder.append(name).append(", "));
@@ -65,7 +109,12 @@ public class Greeting {
                 .toString();
     }
 
-
+    /**
+     * Returns a normal greeting depending on how many names are required to be greeted
+     *
+     * @param lowerCaseNames an List of strings to greet
+     * @return a normal greeting containing the names from the List parameter
+     */
     public String getLowerCaseGreeting(List<String> lowerCaseNames) {
         if (lowerCaseNames.isEmpty()) {
             return "";
@@ -76,14 +125,18 @@ public class Greeting {
         }
         return buildLowerCaseGreeting(lowerCaseNames);
     }
-    
+
+    /**
+     * Builds an normal greeting for more multiple names
+     *
+     * @param lowerCaseNames an array of strings to be included in the greeting
+     * @return a String representing the greeting
+     */
     public String buildLowerCaseGreeting(List<String> lowerCaseNames) {
         StringBuilder stringBuilder = new StringBuilder("Hello, ");
         String lastName = lowerCaseNames.remove(lowerCaseNames.size() - 1);
-        for (String name : lowerCaseNames) {
-            stringBuilder.append(name)
-                    .append(", ");
-        }
+        lowerCaseNames.forEach(name -> stringBuilder.append(name)
+                .append(", "));
         return stringBuilder
                 .append("and ")
                 .append(lastName)
@@ -91,18 +144,38 @@ public class Greeting {
                 .toString();
     }
 
+    /**
+     * Returns a shout greeting for when only one name is used
+     * @param name the name to shout
+     * @return a shout greeting
+     */
     public String shoutSingleName(String name) {
         return "HELLO " + name + "!";
     }
 
+    /**
+     * Returns a normal greeting for when only one name is used
+     * @param name the name to greet
+     * @return a greeting
+     */
     public String saySingleName(String name) {
         return "Hello, " + name + ".";
     }
 
+    /**
+     * Removes first and last character from strip, should be used to remove quotes from escaped names
+     * @param name the name with quotes
+     * @return the name with quotes removed
+     */
     private String stripQuotesOffString(String name) {
         return name.substring(1, name.length() - 1);
     }
 
+    /**
+     * Splits a string by comma
+     * @param name the name with the comma
+     * @return a list of strings split by the comma
+     */
     private List<String> splitNamesByComma(String name) {
         return Arrays.asList(name.split(", "));
     }
--- Greeting/src/test/java/GreetingTest.java
+++ Greeting/src/test/java/GreetingTest.java
@@ -14,80 +14,124 @@ public class GreetingTest {
     }
 
     @Nested
-    @DisplayName("It should greet individuals correctly")
-    class SingleNameGreetingTests {
-
+    @DisplayName("If the name isn't fully capitalised it should return a normal greeting ")
+    class normalGreetingTests {
 
         @Test
-        @DisplayName("It should return a greeting with name")
-        void returnGreetingWithName() {
-            assertEquals("Hello, Bob.", greeting.greet(new String[]{"Bob"}));
+        @DisplayName("It should return a normal greeting with a lowercase name")
+        void itShouldReturnANormalGreetingWithALowercaseName() {
+            assertEquals("Hello, bob.", greeting.greet(new String[]{"bob"}));
         }
 
         @Test
-        @DisplayName("If no name is provided it should return a greeting with a default value")
-        void returnGreetingWithDefaultValue() {
-            assertEquals("Hello, my friend.", greeting.greet(null));
+        @DisplayName("It should return a normal greeting with a name that includes some captial letters")
+        void itShouldReturnGreetingIncludingSomeCapitalLetters() {
+            assertEquals("Hello, BoB.", greeting.greet(new String[]{"BoB"}));
         }
 
         @Test
-        @DisplayName("If name is uppercase return a shout greeting")
-        void returnShoutGreetingIfNameIsUppercase() {
-            assertEquals("HELLO JERRY!", greeting.greet(new String[]{"JERRY"}));
+        @DisplayName("It should return a normal greeting with a lowercase name including numbers and symbols")
+        void itShouldReturnGreetingWithANameIncludingNumbersAndSymbols() {
+            assertEquals("Hello, BoB124698328749*£*$)(\"$£.\".", greeting.greet(new String[]{"BoB124698328749*£*$)(\"$£.\""}));
         }
 
-    }
-
-    @Nested
-    @DisplayName("It should greet multiple names correctly")
-    class MultipleNameGreetingTests {
-
         @Test
-        @DisplayName("If array of two names is passed, return a greeting with both names")
-        void returnGreetingWithTwoNames() {
+        @DisplayName("If an array of two lowercase names is passed, return a normal greeting with both names")
+        void itShouldReturnANormalGreetingWithTwoNames() {
             assertEquals("Hello, Jill and Jane.", greeting.greet(new String[]{"Jill", "Jane"}));
         }
 
         @Test
-        @DisplayName("If multiple names are passed return a comma separated greeting with all names")
-        void returnGreetingWithMultipleNames() {
+        @DisplayName("If multiple lowercase names are passed return a comma separated greeting with all names")
+        void itShouldReturnANormalGreetingWithMultipleNames() {
             assertEquals("Hello, Amy, Brian, and Charlotte.", greeting.greet(new String[]{"Amy", "Brian", "Charlotte"}));
-        }
-
-        @Test
-        @DisplayName("Allow mixing of greetings and shouts")
-        void returnMixedGreetingAndShout() {
-            assertEquals("Hello, Amy and Charlotte. AND HELLO BRIAN!", greeting.greet(new String[]{"Amy", "BRIAN", "Charlotte"}));
-        }
-
-        @Test
-        @DisplayName("Allow mixing of greetings and shouts with two shouts")
-        void returnMixedGreetingAndShoutWithMultipleShouts() {
-            assertEquals("Hello, Amy and Charlotte. AND HELLO BRIAN AND TOM!", greeting.greet(new String[]{"Amy", "BRIAN", "TOM", "Charlotte"}));
-        }
-
-
-        @Test
-        @DisplayName("Allow mixing of greetings and shouts with two shouts")
-        void dsadsa() {
-            assertEquals("Hello, Amy and Charlotte. AND HELLO BRIAN, TOM, AND SAM!", greeting.greet(new String[]{"Amy", "BRIAN", "TOM", "SAM", "Charlotte"}));
         }
 
     }
 
     @Nested
-    @DisplayName("It should split commas in names correctly")
+    @DisplayName("If the name is fully capitalised it should return a shout greeting ")
+    class ShoutGreetingTests {
+        @Test
+        @DisplayName("It should return a shout greeting with a uppercase name")
+        void itShouldReturnAShoutGreetingWithAnUppercaseName() {
+            assertEquals("HELLO JERRY!", greeting.greet(new String[]{"JERRY"}));
+        }
+
+        @Test
+        @DisplayName("It should return a shout greeting with a uppercase name containing numbers and symbols")
+        void itShouldReturnAShoutGreetingIncludingNumbersAndSymbols() {
+            assertEquals("HELLO JERRY124698328749*£*$)(\"$£.\"!", greeting.greet(new String[]{"JERRY124698328749*£*$)(\"$£.\""}));
+        }
+
+        @Test
+        @DisplayName("It should return a shout greeting with two names separated with \"AND\"")
+        void itShouldReturnAShoutGreetingWithTwoNames() {
+            assertEquals("HELLO TOM AND JERRY!", greeting.greet(new String[]{"TOM", "JERRY"}));
+        }
+
+        @Test
+        @DisplayName("It should return a shout greeting with multiple names separated with commas")
+        void itShouldReturnAShoutGreetingWithMultipleNames() {
+            assertEquals("HELLO TOM, JERRY, AND SPIKE!", greeting.greet(new String[]{"TOM", "JERRY", "SPIKE"}));
+        }
+    }
+
+
+    @Nested
+    @DisplayName("A Greeting can consist of a mix of both normal greetings and shout greetings")
+    class MixedGreetingTests {
+        @Test
+        @DisplayName("Allow mixing of a single normal greeting and a single shout")
+        void itShouldReturnAMixOfOneNormalAndOneShoutGreeting() {
+            assertEquals("Hello, Amy. AND HELLO BRIAN!", greeting.greet(new String[]{"Amy", "BRIAN"}));
+        }
+
+        @Test
+        @DisplayName("Allow mixing of a two normal greetings and two shouts")
+        void itShouldReturnAMixOfTwoNormalAndTwoShoutGreeting() {
+            assertEquals("Hello, Amy and Charlotte. AND HELLO BRIAN AND TOM!", greeting.greet(new String[]{"Amy", "BRIAN","Charlotte","TOM"}));
+        }
+
+        @Test
+        @DisplayName("Allow mixing of a multiple normal greetings and multiple shouts")
+        void itShouldReturnAMixOfMultipleNormalAndMultipleShoutGreeting() {
+            assertEquals("Hello, Amy, Steve, Fred, and Charlotte. AND HELLO BRIAN, TOM, AND TONY!", greeting.greet(new String[]{"Amy", "BRIAN", "Steve","Fred","TOM", "Charlotte","TONY"}));
+        }
+    }
+
+    @Nested
+    @DisplayName("If any names contain an unescaped comma then the it should be split with the comma as the delimiter")
     class SplitNameGreetingTests {
         @Test
-        @DisplayName("It should split strings containing commas")
-        void splitGreetingsByCommas() {
+        @DisplayName("It should split normal greetings containing commas")
+        void itShouldSplitNormalGreetingsByComma() {
+            assertEquals("Hello, Bob, Charlie, and Dianne.", greeting.greet(new String[]{"Bob", "Charlie, Dianne"}));
+        }
+
+        @Test
+        @DisplayName("It should split shout greetings containing containing commas")
+        void itShouldSplitShoutGreetingsByComma() {
+            assertEquals("HELLO TOM, JERRY, AND BOB!", greeting.greet(new String[]{"TOM, JERRY","BOB"}));
+        }
+
+        @Test
+        @DisplayName("It should split strings containing  multiple commas")
+        void itShouldSplitGreetingsWithMultipleCommas() {
             assertEquals("Hello, Bob, Charlie, and Dianne.", greeting.greet(new String[]{"Bob", "Charlie, Dianne"}));
         }
 
         @Test
         @DisplayName("It should not split strings which contain commas which are also surrounded by quotes")
-        void dontSplitGreetingsSurroundedByQuotes() {
-            assertEquals("Hello, Bob and Charlie, Dianne.", greeting.greet(new String[]{"Bob", "\"Charlie, Dianne\""}));
+        void itShouldNotSplitGreetingsWithEscapedCommas() {
+            assertEquals("Hello, Bob, Tony, Steve, and Charlie, Dianne.", greeting.greet(new String[]{"Bob", "Tony, Steve", "\"Charlie, Dianne\""}));
         }
+
+    }
+
+    @Test
+    @DisplayName("If no name is provided, it should return a greeting with a default value")
+    void itShouldReturnADefaultValueIfInputIsNull() {
+        assertEquals("Hello, my friend.", greeting.greet(null));
     }
 }
